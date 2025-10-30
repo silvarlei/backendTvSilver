f11a4dbd3d4327b16b27173f00cf1bc4415f2bcd
silvarlei <silvarleisoares@hotmail.com>
2025-10-29 21:06:07 -0300

    3
    3

--- main.py
+++ main.py
@@ -4,42 +4,39 @@ import requests
 
 app = FastAPI()
 
-VIDEO_URL = "http://solard2.metag.click:80/movie/729767765/551952986/4667973.mp4"  # substitua pelo seu link real
+# Link direto para o vídeo MP4
+VIDEO_URL_MP4 = "http://solard2.metag.click:80/movie/729767765/551952986/4667973.mp4"
+
+# Link direto para o vídeo TS (HLS)
+VIDEO_URL_TS = "http://saga10.pro:80/345715/562156/2639316"
 
 @app.get("/video")
-def stream_video():
+def stream_mp4_video():
     try:
         headers = {
             "User-Agent": "Mozilla/5.0"
         }
 
-        video_response = requests.get(VIDEO_URL, stream=True, headers=headers, timeout=15)
+        video_response = requests.get(VIDEO_URL_MP4, stream=True, headers=headers, timeout=15)
 
         if video_response.status_code != 200:
             return StreamingResponse(
-                content=iter([f"Erro ao acessar o vídeo: {video_response.status_code}".encode()]),
+                content=iter([f"Erro ao acessar o vídeo MP4: {video_response.status_code}".encode()]),
                 status_code=502,
                 media_type="text/plain"
             )
 
         return StreamingResponse(
             video_response.iter_content(chunk_size=1024),
-            media_type="video/MP2T",
-            headers={
-                "Content-Disposition": "inline; filename=video.ts"
-            }
+            media_type="video/mp4"
         )
 
     except Exception as e:
         return StreamingResponse(
-            content=iter([f"Erro interno: {str(e)}".encode()]),
+            content=iter([f"Erro interno MP4: {str(e)}".encode()]),
             status_code=500,
             media_type="text/plain"
         )
-
-
-# Link direto para o vídeo .ts
-VIDEO_URL_TS = "http://saga10.pro:80/345715/562156/2639316"  # substitua pelo seu link real
 
 @app.get("/tv")
 def stream_ts_video():
@@ -52,22 +49,19 @@ def stream_ts_video():
 
         if video_response.status_code != 200:
             return StreamingResponse(
-                content=iter([f"Erro ao acessar o vídeo: {video_response.status_code}".encode()]),
+                content=iter([f"Erro ao acessar o vídeo TS: {video_response.status_code}".encode()]),
                 status_code=502,
                 media_type="text/plain"
             )
 
         return StreamingResponse(
             video_response.iter_content(chunk_size=1024),
-            media_type="video/MP2T",
-            headers={
-                "Content-Disposition": "inline; filename=video.ts"
-            }
+            media_type="video/MP2T"
         )
 
     except Exception as e:
         return StreamingResponse(
-            content=iter([f"Erro interno: {str(e)}".encode()]),
+            content=iter([f"Erro interno TS: {str(e)}".encode()]),
             status_code=500,
             media_type="text/plain"
         )
